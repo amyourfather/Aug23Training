886a20f09a1c40be8d96ed0b7524034746290a21
Benjamin Xu <yxu117@berkeley.edu>
2021-09-21 17:06:17 -0700

    added edge

--- basics/src/test/java/framework/Testbase.java
+++ basics/src/test/java/framework/Testbase.java
@@ -1,5 +1,7 @@
 package framework;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.time.Duration;
 
 import org.junit.After;
@@ -16,12 +18,12 @@ public abstract class Testbase {
 	private final String WEB_DRIVER_SYSTEM_PROPERTY = "webdriver.chrome.driver"; 
 	protected ChromeDriverDocPage ChromeDriverDocPage;
 	@Before
-	public void PreinitChorme() {	
+	public void PreinitChorme() throws IOException, URISyntaxException {	
 		
 		//will fix later
-		ChromeDriverPath = "C:\\Users\\techo\\Desktop\\Selenium\\chromedriver.exe";
-		
-		System.setProperty(WEB_DRIVER_SYSTEM_PROPERTY, ChromeDriverPath);
+		//ChromeDriverPath = "C:\\Users\\techo\\Desktop\\Selenium\\chromedriver.exe";
+		//ChromeDriverPath = "C:\\Users\\techo\\git\\Aug23Training\\basics\\src\\test\\resources";
+		//System.setProperty(WEB_DRIVER_SYSTEM_PROPERTY, ChromeDriverPath);
 		DManager = WebDriverFactory.getManager("Chrome");
 		DManager.createDriver();
 		driver = DManager.getDriver();
@@ -30,6 +32,6 @@ public abstract class Testbase {
 	
 	@After
 	public void cleanup(){
-		DManager.quitDriver();
+		//DManager.quitDriver();
 	}
 }
